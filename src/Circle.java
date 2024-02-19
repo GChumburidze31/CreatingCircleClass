e0b7546f57d4e20cf9383485c4a66424bb562ab6
GioC8785 <giowatt81@gmail.com>
2024-02-19 16:09:59 -0500

    First Class in OOP!

---
+++ src/Circle.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+// define the class
+class Circle {
+    // attributes or instance variables, default values of 0, and 0.0
+    Point center;
+    double radius;
+    double getPerimeter(){
+        return 2 * Math.PI * radius;
+    }
+    double getArea (){
+        return Math.PI * radius * radius;
+    }
+    void setRadius (double newRadius){
+        radius = newRadius;
+    }
+    void setCenter(Point newCenter){
+        center = newCenter;
+    }
+}
